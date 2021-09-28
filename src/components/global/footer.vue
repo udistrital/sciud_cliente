1ad6f1a7b7c4b6d2a32f21038145ab6fe5dc7421
Diego Vargas <dev@nemedi.com>
2021-09-28 14:00:02 -0500

    feat: Activación y desactivación de estructuras

--- src/components/global/footer.vue
+++ src/components/global/footer.vue
@@ -9,7 +9,7 @@
 
 		<div class="navbar-collapse collapse" id="navbar-footer">
 			<span class="navbar-text">
-				SICIUD v2.0.20210921.p - ©{{ year }}
+				SICIUD v2.0.20210928.p - ©{{ year }}
 				<a href="http://cidc.udistrital.edu.co" target="_blank" title="Centro de Investigaciones y Desarrollo Científico..."
 					>Centro de Investigaciones y Desarrollo Científico (CIDC)</a
 				>
